e3841c45c7d5f3fba6620ba4a6021343193e26f7
Javas <javasaryadinata24@gmail.com>
2024-03-26 18:26:05 +0700

    Latest Update

--- lib/main.dart
+++ lib/main.dart
@@ -11,16 +11,16 @@ class RecipeApp extends StatelessWidget {
   static const MaterialColor white = MaterialColor(
     0xFFFFFFFFF,
     <int, Color>{
-      50: Color(0xFFFFFFFFF),
-      100: Color(0xFFFFFFFFF),
-      200: Color(0xFFFFFFFFF),
-      300: Color(0xFFFFFFFFF),
-      400: Color(0xFFFFFFFFF),
-      500: Color(0xFFFFFFFFF),
-      600: Color(0xFFFFFFFFF),
-      700: Color(0xFFFFFFFFF),
-      800: Color(0xFFFFFFFFF),
-      900: Color(0xFFFFFFFFF),
+      50: Color(0xFFFFFFFF),
+      100: Color(0xFFFFFFFF),
+      200: Color(0xFFFFFFFF),
+      300: Color(0xFFFFFFFF),
+      400: Color(0xFFFFFFFF),
+      500: Color(0xFFFFFFFF),
+      600: Color(0xFFFFFFFF),
+      700: Color(0xFFFFFFFF),
+      800: Color(0xFFFFFFFF),
+      900: Color(0xFFFFFFFF),
     },
   );
 
